aa4398970baa082864736dc2df329b9557c84873
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-06 18:52:31 -0300

    feat: add Catalog pag

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,27 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogoPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+  return (
+    <div className="container mx-auto max-w-[80rem] p-5">
+      <Badge
+        className="gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8 pt-5">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CatalogoPage;
--- src/components/ui/containerLayout.tsx
+++ src/components/ui/containerLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const ContainerLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="mx-auto w-full max-w-7xl px-5">{children}</div>;
+  return <div className="mx-auto w-full max-w-7xl p-5">{children}</div>;
 };
 
 export default ContainerLayout;
