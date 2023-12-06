525875f61140b5a46aff83cb8a81bef64068c3d1
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-06 20:02:00 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Card } from "./card";
 import { Button } from "./button";
 import {
@@ -12,7 +13,13 @@ import {
   LogOut,
 } from "lucide-react";
 
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
@@ -89,10 +96,17 @@ const Header = () => {
               <PercentIcon size={16} />
               Ofertas
             </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href={`/catalog`}>
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
