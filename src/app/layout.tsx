8d6e7121df7319ba03de9424d6b8add1e5571be9
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-06 16:56:04 -0300

    feat: add Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
+import Footer from "@/components/ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <AuthProvider>
           <Header />
           {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="mt-8 bg-accent px-8 py-4 text-sm opacity-75">
+      © 2023 Copyright <span className="text-bold">DSMSTORE</span>
+    </div>
+  );
+};
+
+export default Footer;
