31c0e5321ce4c7a7bcc23e805c54ad549b89cb18
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-06 20:09:41 -0300

    feat: ensure user goes to category page on category item click

--- src/app/(home)/components/categoory-item.tsx
+++ src/app/(home)/components/categoory-item.tsx
@@ -9,6 +9,7 @@ import {
   SpeakerIcon,
   SquareIcon,
 } from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -25,13 +26,15 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
   };
   return (
     <ContainerLayout>
-      <Badge
-        variant="outline"
-        className="flex items-center justify-center gap-2 rounded-lg py-3"
-      >
-        {categoryIcon[category.slug as keyof typeof categoryIcon]}
-        <span className="text-xs font-semibold">{category.name}</span>
-      </Badge>
+      <Link href={`/category/${category.slug}`}>
+        <Badge
+          variant="outline"
+          className="flex items-center justify-center gap-2 rounded-lg py-3"
+        >
+          {categoryIcon[category.slug as keyof typeof categoryIcon]}
+          <span className="text-xs font-semibold">{category.name}</span>
+        </Badge>
+      </Link>
     </ContainerLayout>
   );
 };
