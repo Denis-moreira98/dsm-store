b9d92862b7b1b8077febaefd7d3725db9282700c
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-07 15:34:36 -0300

    style: adjust responsiveness desktop

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 
 import { ProductWithTotalPrice } from "@/helpers/product";
-import {
-  ArrowDown,
-  ArrowLeftIcon,
-  ArrowRightIcon,
-  TruckIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon, TruckIcon } from "lucide-react";
 import { useState } from "react";
 
 interface ProductInfoProps {
@@ -33,60 +27,67 @@ const ProductInfo = ({
   };
 
   return (
-    <div className="flex flex-col px-5">
-      <h2 className="txt-lg">{name}</h2>
+    <div className="flex flex-col px-5 lg:w-[40%] lg:rounded-lg lg:bg-accent lg:p-10">
+      <div className="flex justify-between">
+        <h2 className="text-lg lg:text-2xl">{name}</h2>
+      </div>
 
       <div className="flex items-center gap-2">
-        <h1 className="text-xl font-bold">R${totalPrice.toFixed(2)}</h1>
+        <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
         {discountPercentage > 0 && (
           <DiscountBadge>{discountPercentage}</DiscountBadge>
         )}
       </div>
       {discountPercentage > 0 && (
         <p className="text-sm line-through opacity-75">
-          R${Number(basePrice).toFixed(2)}
+          De: R$ {Number(basePrice).toFixed(2)}
         </p>
       )}
+
       <div className="mt-4 flex items-center gap-2">
         <Button
           size="icon"
           variant="outline"
           onClick={handleDecreaseQuantityClick}
         >
-          <ArrowLeftIcon size={16} />
+          <ChevronLeftIcon size={16} />
         </Button>
+
         <span>{quantity}</span>
+
         <Button
           size="icon"
           variant="outline"
           onClick={handlIncreaseQuantityClick}
         >
-          <ArrowRightIcon size={16} />
+          <ChevronRightIcon size={16} />
         </Button>
       </div>
-      <div className="flex flex-col gap-3">
-        <h3 className="font-bold">Descrição:</h3>
-        <p className="justify-center text-justify text-sm opacity-60">
-          {description}
-        </p>
+
+      <div className="mt-8 flex flex-col gap-3">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-justify text-sm opacity-60">{description}</p>
       </div>
+
       <Button className="mt-8 font-bold uppercase">
         Adicionar ao carrinho
       </Button>
 
-      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
-        <div className="items center flex gap-1">
+      <div className="mt-6 flex items-center justify-between rounded-lg bg-accent px-5 py-2 lg:bg-[#2A2A2A]">
+        <div className="flex items-center gap-2">
           <TruckIcon />
+
           <div className="flex flex-col">
-            <p>
-              Entreg via <span className="font-bold">FSPacket</span>
+            <p className="text-xs">
+              Entrega via <span className="font-bold">FSPacket</span>
             </p>
-            <p className="text-xs text-primary">
-              Envio pra <span className="font-bold">todo Brasil</span>
+            <p className="text-xs text-[#8162ff]">
+              Envio para <span className="font-bold">todo Brasil</span>
             </p>
           </div>
         </div>
-        <p className="text-xs font-bold">Frete grátis</p>
+
+        <p className="text-xs font-bold">Frete Grátis</p>
       </div>
     </div>
   );
