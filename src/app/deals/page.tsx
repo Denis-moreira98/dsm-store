31c316e761bdf0d74c184b7fd3e8b907c1f2abe8
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-10 13:09:20 -0300

    feat: add metadata to Deals page

--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -4,6 +4,12 @@ import ProductItem from "@/components/ui/product-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 import { PercentIcon } from "lucide-react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DSM STORE - Ofertas",
+  description: "Produtos em ofertas",
+};
 
 const DealsPage = async () => {
   const deals = await prismaClient.product.findMany({
